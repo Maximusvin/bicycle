e435c688e3fa46168735f6fc1bbd2dba57f231fd
Maxim Kozlov <Maximusvin@gmail.com>
2021-07-24 10:45:29 +0300

    Create section statistics

---
+++ src/components/Statistics/Statistics.style.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import { COLORS } from 'assets/colors';
+
+export const Wrapper = styled.div`
+  border-top: 1px solid ${COLORS.greyLine};
+  padding-top: 8px;
+  color: ${COLORS.black};
+`;
+
+export const Title = styled.h2`
+  font-size: 18px;
+  line-height: 28px;
+  font-weight: 700;
+  text-transform: uppercase;
+  margin-bottom: 10px;
+`;
+
+export const Line = styled.p`
+  font-size: 14px;
+  line-height: 22px;
+  margin-bottom: 10px;
+`;
+
+export const Span = styled.span`
+  font-weight: 700;
+`;
