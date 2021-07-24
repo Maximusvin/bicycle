c053860ff672dce4c19d9f0d4c2dc01f900b1ce7
Maxim Kozlov <Maximusvin@gmail.com>
2021-07-25 00:41:11 +0300

    Create fetch

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -1,5 +1,10 @@
+import { useEffect } from 'react';
 import { Main } from 'components';
+import { fetchBicycles } from 'services/bicycles-api';
 
 export const MainView = () => {
+  useEffect(() => {
+    fetchBicycles().then(res => console.log(res));
+  }, []);
   return <Main />;
 };
